6e743663a0c90b8b00a961f415c1259c227a81d2
Jaime Farfan NET <jfarfannet@gmail.com>
2019-11-06 05:23:16 -0500

    Primer commit

---
+++ SobrecargaUnario/cmake-build-debug/CMakeFiles/SobrecargaUnario.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SobrecargaUnario.dir/main.cpp.obj"
+  "SobrecargaUnario.exe"
+  "SobrecargaUnario.exe.manifest"
+  "SobrecargaUnario.pdb"
+  "libSobrecargaUnario.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SobrecargaUnario.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
