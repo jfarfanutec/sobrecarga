5bde9db56ee05bc656b7d5e0214672464a10395e
Jaime Farfan <jfarfan@utec.edu.pe>
2019-11-06 06:04:03 -0500

    Clase Friend

---
+++ FuncionesAmigas/cmake-build-debug/CMakeFiles/FuncionesAmigas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FuncionesAmigas.dir/main.cpp.obj"
+  "FuncionesAmigas.exe"
+  "FuncionesAmigas.exe.manifest"
+  "FuncionesAmigas.pdb"
+  "libFuncionesAmigas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FuncionesAmigas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ FuncionesAmigas/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Rectangulo.h"
+#include "Cuadrado.h"
+
+int main() {
+    Rectangulo r;
+    Cuadrado c;
+
+    c.mostrar(r);
+
+    return 0;
+}
